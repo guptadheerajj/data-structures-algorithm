be6e85427995b4d0132dd441d09548891f65cd9b
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2026-01-12 23:55:37 +0530

    implement merge sort (recursion)

---
+++ sorting/merge-sort/mergeSort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high) {
+	vector<int> temp;
+	int left = low;
+	int right = mid + 1;
+
+	while (left <= mid && right <= high) {
+		if (arr[left] < arr[right]) {
+			temp.push_back(arr[left]);
+			left++;
+		} else {
+			temp.push_back(arr[right]);
+			right++;
+		}
+	}
+
+	while (left <= mid) {
+		temp.push_back(arr[left]);
+		left++;
+	}
+
+	while (right <= high) {
+		temp.push_back(arr[right]);
+		right++;
+	}
+
+	for (int i = low; i <= high; i++) {
+		arr[i] = temp[i - low];
+	}
+}
+
+void mergeSort(int arr[], int low, int high) {
+	if (low >= high) {
+		return;
+	}
+	int mid = (low + high) / 2;
+	mergeSort(arr, low, mid);
+	mergeSort(arr, mid + 1, high);
+	merge(arr, low, mid, high);
+}
+
+int main() {
+	int arr[] = {3, 2, 5, 1, 4, 9, 7, 8, 0, 6, -1};
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	mergeSort(arr, 0, size);
+
+	for (auto it : arr) {
+		cout << it << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
