8f2cf5b180d6a06e71974df615ec4a85698e2f86
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2026-01-18 20:08:44 +0530

    solve 189. Rotate Array with better and best approaches

--- Arrays/easy/rotateArray/rotateArray.cpp
+++ Arrays/easy/rotateArray/rotateArray.cpp
@@ -34,49 +34,50 @@ using namespace std;
 // 	}
 // }
 
+// using temporary space of nums - k
+// void rotate(vector<int>& nums, int k) {
+// 	int size = nums.size();
+// 	k = k % size;
+// 	vector<int> temp;
+
+// 	for (int i = 0; i < size - k; i++) {
+// 		temp.push_back(nums[i]);
+// 	}
+
+// 	for (int i = size - k; i < size; i++) {
+// 		nums[i - (size - k)] = nums[i];
+// 	}
+
+// 	for (int i = k; i < size; i++) {
+// 		nums[i] = temp[i - k];
+// 	}
+// }
+
+// best: space -> O(1), time -> O(2n) [(n-d) + (d) + (n)]
+// void rotate(vector<int>& nums, int k) {
+// 	k = k % nums.size();
+// 	reverse(nums.begin(), nums.end() - k);
+// 	reverse(nums.end() - k, nums.end());
+// 	reverse(nums.begin(), nums.end());
+// }
+
+// by using custom reverse function
+void myReverse(vector<int>& nums, int start, int end) {
+	while (start <= end) {
+		int temp = nums[start];
+		nums[start] = nums[end];
+		nums[end] = temp;
+		start++;
+		end--;
+	}
+}
+
 void rotate(vector<int>& nums, int k) {
 	int size = nums.size();
 	k = k % size;
-	int dividingIndex = size - k;
-
-	for (int i = 0; i < dividingIndex / 2; i++) {
-		int temp = nums[i];
-		nums[i] = nums[dividingIndex - 1 - i];
-		nums[dividingIndex - 1 - i] = temp;
-	}
-
-	for (int i = dividingIndex, j = size - 1; i < dividingIndex - k / 2; i++) {
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
-		j--;
-	}
-
-	for (int i = 0; i < size / 2; i++) {
-		int temp = nums[i];
-		nums[i] = nums[size - 1 - i];
-		nums[size - 1 - i] = temp;
-	}
-
-	// int size = nums.size();
-	// int low = 0;
-	// k = k % size;
-	// if (k == 0) {
-	// 	return;
-	// }
-	// int high = size - k;
-
-	// while (low < size - 1) {
-	// 	int temp = nums[low];
-	// 	nums[low] = nums[high];
-	// 	nums[high] = temp;
-
-	// 	high++;
-	// 	if (high >= size) {
-	// 		high = size - k;
-	// 	}
-	// 	low++;
-	// }
+	myReverse(nums, 0, size - k - 1);
+	myReverse(nums, size - k, size - 1);
+	myReverse(nums, 0, size - 1);
 }
 
 int main() {
@@ -113,55 +114,55 @@ int main() {
 	cout << endl << endl;
 
 	// Test case 2: Rotate by 2
-	// vector<int> nums2 = {-1, -100, 3, 99};
-	// cout << "Test case 2:" << endl;
-	// cout << "Original array: ";
-	// for (int num : nums2) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl;
+	vector<int> nums2 = {-1, -100, 3, 99};
+	cout << "Test case 2:" << endl;
+	cout << "Original array: ";
+	for (int num : nums2) {
+		cout << num << " ";
+	}
+	cout << endl;
 
-	// k = 2;
-	// rotate(nums2, k);
-	// cout << "After rotating by " << k << ": ";
-	// for (int num : nums2) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl << endl;
+	k = 2;
+	rotate(nums2, k);
+	cout << "After rotating by " << k << ": ";
+	for (int num : nums2) {
+		cout << num << " ";
+	}
+	cout << endl << endl;
 
 	// // Test case 3: k greater than array length
-	// vector<int> nums3 = {1, 2, 3};
-	// cout << "Test case 3:" << endl;
-	// cout << "Original array: ";
-	// for (int num : nums3) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl;
+	vector<int> nums3 = {1, 2, 3};
+	cout << "Test case 3:" << endl;
+	cout << "Original array: ";
+	for (int num : nums3) {
+		cout << num << " ";
+	}
+	cout << endl;
 
-	// k = 5;
-	// rotate(nums3, k);
-	// cout << "After rotating by " << k << " (k > n): ";
-	// for (int num : nums3) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl << endl;
+	k = 5;
+	rotate(nums3, k);
+	cout << "After rotating by " << k << " (k > n): ";
+	for (int num : nums3) {
+		cout << num << " ";
+	}
+	cout << endl << endl;
 
 	// // Test case 4: Single element
-	// vector<int> nums4 = {1};
-	// cout << "Test case 4:" << endl;
-	// cout << "Original array: ";
-	// for (int num : nums4) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl;
+	vector<int> nums4 = {1};
+	cout << "Test case 4:" << endl;
+	cout << "Original array: ";
+	for (int num : nums4) {
+		cout << num << " ";
+	}
+	cout << endl;
 
-	// k = 1;
-	// rotate(nums4, k);
-	// cout << "After rotating by " << k << ": ";
-	// for (int num : nums4) {
-	// 	cout << num << " ";
-	// }
-	// cout << endl;
+	k = 1;
+	rotate(nums4, k);
+	cout << "After rotating by " << k << ": ";
+	for (int num : nums4) {
+		cout << num << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
